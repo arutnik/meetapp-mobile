f8a6a455952b828c27dfc87601246a749c4844b5
Andres Rutnik <andresrutnik@gmail.com>
2015-03-03 15:27:57 -0500

    Basic models check in

---
+++ src/Meetapp.Mobile.Core/Models/Meet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetapp.Mobile.Core.Models
+{
+    /// <summary>
+    /// This models the basic properties of a Meet. 
+    /// </summary>
+    public class Meet
+    {
+        /// <summary>
+        /// The user that is hosting the Meet.
+        /// </summary>
+        public UserRef HostUser { get; set; }
+    }
+}
---
+++ src/Meetapp.Mobile.Core/Models/UserRef.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetapp.Mobile.Core.Models
+{
+    /// <summary>
+    /// A lightweight reference to a user.
+    /// </summary>
+    public class UserRef
+    {
+        /// <summary>
+        /// The User's ID. TODO: type as guid?
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// URI to a thumbnail for the user's display picture.
+        /// </summary>
+        public string ThumbnailUri { get; set; }
+
+        /// <summary>
+        /// The name to display for the user.
+        /// </summary>
+        public PersonName DisplayName { get; set; }
+    }
+}
